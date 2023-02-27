2320a4a191a5e6e7ef4f2a341a965989a2d92f15
Dmytro Syvenko <syvenkod@gmail.com>
2023-02-27 10:59:51 +0200

    fix form for appointment dialog

--- src/app/components/appointment-dialog/appointment-dialog.component.ts
+++ src/app/components/appointment-dialog/appointment-dialog.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { CommonService } from 'src/app/service/common.service';
 
 @Component({
@@ -7,9 +7,21 @@ import { CommonService } from 'src/app/service/common.service';
   templateUrl: './appointment-dialog.component.html',
   styleUrls: ['./appointment-dialog.component.scss']
 })
-export class AppointmentDialogComponent {
-  constructor(private service:CommonService) { }
+export class AppointmentDialogComponent implements OnInit {
+  constructor(private service:CommonService,
+    private appointForm: FormBuilder) { }
   appointmentData: Date = new Date();
   form: FormGroup;
 
+ngOnInit() {
+    this.form = this.appointForm.group({
+      title: [null, [Validators.required]],
+      // time: [null],
+      description: [""]
+    })
+  }
+  onSubmit(form:any) {
+    console.log(form.value);
+    this.form.reset();
+  }
 }
